13dc9169dd9405f594ed80bb0dffab0b1038d34f
noname0000-blip <augusteide98@hotmail.com>
2025-03-25 14:50:54 +0100

    Update model.js

--- Utility/model.js
+++ Utility/model.js
@@ -1,3 +1,70 @@
 const model = {
+    app: {
+        currentPage: '',
+        currentUser: 0
+    },
+    input: {
+        loginPage: {
+            username: '',
+            password: '',
+            rememberMe: false,
+        }
+    },
+    data: {
+        loginPage: {
+            username: '',
+            password: '',
+            rememberMe: false,
+        },
+        users: [
+            {
+                id: 0,
+                username: '',
+                password: '',
+                groupsId: [0],
+                privateLists: [{
+                    listType: '',
+                    listName: '',
+                    listId: '',
+                    private: true,
+                }],
+                recentList: 1,
+                shoppingLog: [{}],
+            },
+            {
+                id: 1,
+                username: '',
+                password: '',
+                groupsId: '',
+                privateLists: '',
+                recentList: '',
+                shoppingLog: '',
+            }
+        ],
+        groups: [
+            {
+                id: 0,
+                name: 'Rubber Duck As',
+                usersId: [0, 1]
+            },
+            {
+                id: 1,
+                name: 'Family group',
+                usersId: [0, 1]
+            }
+        ],
+        dashBoard: {
+            id: '',
+        },
+        privateLists: {
 
+        },
+
+        shoppingList: {
+            items: [{ name: '', amount: '' }],
+        },
+        favoriteItems: [
+            { name: '', amount: '1' }
+        ],
+    }
 }
