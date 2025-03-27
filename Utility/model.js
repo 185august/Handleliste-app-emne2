121dea87b0de8e85061681d6a5227944533dfd86
noname0000-blip <augusteide98@hotmail.com>
2025-03-27 11:49:02 +0100

    model v2

--- Utility/model.js
+++ Utility/model.js
@@ -39,115 +39,197 @@ const model = {
         }
     },
     data: {
-        users: [
-            {
-                userId: 0,
-                username: '',
-                password: '',
-                groupsId: [0],
-                recentListId: 0,
-                rememberMe: false,
-                privateListId: [0],
-                privateWishListId: [0]
-            }
-        ],
-        groups: [
-            {
-                groupId: 0,
-                name: 'Rubber Duck As',
-                usersId: [0, 1],
-                adminUserId: [0],
-                groupShoppingListId: [0],
-                groupWishListId: [0]
-                //groupList (referanse eller ha det rett her?)
-            },
-            {
-                groupId: 1,
-                name: 'Family group',
-                usersId: [0, 1],
-                adminUserId: [0],
-                groupShoppingListId: [1],
-                groupWishListId: [1]
-                //groupList (referanse eller ha det rett her?)
-            }
-        ],
-        lists: [{
-            shoppingList: {
-                groupLists: [{
-                    groupId: 0,
-                    listName: 'handle til bursdag',
-                    groupShoppingListId: 0,
-                    usersCanView: [0, 1],
-                    usersCanEdit: [0],
-                    isCompleted: false,
-                    listItems: [{
-                        itemId: 0,
-                        name: '',
-                        amount: '',
-                        hasBeenBought: false
-                    }]
-                }],
-                privateLists: [{
+        users:
+            [
+                {
                     userId: 0,
-                    listName: 'handle til bursdag',
-                    privateShoppingListId: 0,
-                    isCompleted: false,
-                    listItems: [{
-                        itemId: 0,
-                        name: '',
-                        amount: '',
-                        hasBeenBought: false
-                    }]
-                }],
-            },
-            wishList: {
-                groupLists: [{
+                    username: '',
+                    password: '',
+                    groupsId: [0],
+                    recentListId: 0,
+                    rememberMe: false,
+
+                    lists:
+                        [
+                            {
+                                listId: 0,
+                                listType: 'shoppingList',
+                                isPrivate: true,
+                                listName: 'handle til bursdag',
+                                isCompleted: false,
+                                listItems: [
+                                    {
+                                        itemId: 0,
+                                        name: 'Melk',
+                                        amount: 1,
+                                        price: null,
+                                        hasBeenBought: false,
+                                        whoIsTheRecipient: null,
+                                    },
+                                    {
+                                        itemId: 1,
+                                        name: 'Smør',
+                                        amount: 1,
+                                        price: null,
+                                        hasBeenBought: false,
+                                        whoIsTheRecipient: null,
+                                    }
+                                ],
+                            },
+                            {
+                                listId: 1,
+                                listType: 'wishlist',
+                                isPrivate: false,
+                                listName: 'Ønske liste',
+                                usersCanView: [0, 1],
+                                isCompleted: false,
+                                listItems: [
+                                    {
+                                        itemId: 0,
+                                        name: 'Sykkel',
+                                        amount: 1,
+                                        price: 10000,
+                                        hasBeenBought: false,
+                                        whoIsTheRecipient: ''
+                                    }
+                                ],
+                            }
+                        ],
+
+                    favoriteItemsList: [{
+                        listType: 'favoriteList',
+                        favoriteItems: [
+                            {
+                                itemId: null,
+                                name: 'Melk',
+                                price: null,
+                                whoIsTheRecipient: null,
+                                amountRecentlyBought: 5,
+                                rank: 3
+                            }],
+                    }],
+
+                    listLog: {
+                        listType: 'shoppingList',
+                        listName: 'handle til bursdag',
+                        listId: 0,
+                        date: '26/3/2025',
+                        listItems: [
+                            {
+                                itemId: 0,
+                                name: 'Kake',
+                                amount: 1,
+                                price: null,
+                                hasBeenBought: false,
+                                whoIsTheRecipient: null,
+                            },
+                            {
+                                itemId: 1,
+                                name: 'Coca cola',
+                                amount: 4,
+                                price: null,
+                                hasBeenBought: false,
+                                whoIsTheRecipient: null,
+                            }],
+                    }
+                },
+            ],
+
+        groups:
+            [
+                {
                     groupId: 0,
-                    listName: 'handle til bursdag',
-                    groupWishListId: 0,
-                    usersCanView: [0, 1],
-                    usersCanEdit: [0],
-                    isCompleted: false,
-                    listItems: [{
-                        itemId: 0,
-                        name: '',
-                        amount: '',
-                        hasBeenBought: false,
-                        whoIsTheRecipient: ''
-                    }]
-                }],
-                privateLists: [{
-                    userId: 0,
-                    listName: 'handle til bursdag',
-                    privateWishListId: 0,
-                    usersCanView: [0, 1],
-                    isCompleted: false,
-                    listItems: [{
-                        itemId: 0,
-                        name: '',
-                        amount: '',
-                        hasBeenBought: false,
-                        whoIsTheRecipient: ''
-                    }]
-                }],
-            },
-            favoriteItemsList: [{
-                userId: 0,
-                favoriteItemsListId: 0,
-                favoriteItems: [{
-                    itemId: null,
-                    name: 'Melk',
-                    amountRecentlyBought: 5,
-                    rank: 3
-                }],
-            }],
-        },
-        ],
-        listLog: [{
-            userId: 0,
-            listName: 'handle til bursdag',
-            privateShoppingListId: 0,
-            date: '26/3/2025',
-        }]
+                    name: 'Rubber Duck As',
+                    usersId: [0, 1],
+                    adminUserId: [0],
+                    shoppingLists: [{
+                        groupShoppingListId: 0,
+                        listName: 'handle til jobbfest',
+                        usersCanView: [0, 1],
+                        usersCanEdit: [0],
+                        isCompleted: false,
+                        listItems: [
+                            {
+                                itemId: 0,
+                                name: 'pizza',
+                                amount: 3,
+                                price: null,
+                                hasBeenBought: false,
+                                whoIsTheRecipient: null,
+                            }]
+                    }],
+                    lists: [
+                        {
+                            listId: 0,
+                            listType: 'wishlist',
+                            listName: 'ting vi trenger på jobben',
+                            usersCanView: [0, 1],
+                            usersCanEdit: [0],
+                            isCompleted: false,
+                            listItems: [
+                                {
+                                    itemId: 0,
+                                    name: 'Ny skriver',
+                                    amount: 1,
+                                    price: 10000,
+                                    hasBeenBought: false,
+                                    whoIsTheRecipient: null,
+                                }]
+                        }
+                    ]
+                },
+                {
+                    groupId: 1,
+                    name: 'Family group',
+                    usersId: [0, 1],
+                    adminUserId: [0],
+                    lists: [
+                        {
+                            listId: 0,
+                            listType: 'shoppingList',
+                            listName: 'handle til bursdag',
+                            usersCanView: [0, 1],
+                            usersCanEdit: [0],
+                            isCompleted: false,
+                            listItems: [
+                                {
+                                    itemId: 0,
+                                    name: 'iskrem',
+                                    amount: 5,
+                                    price: null,
+                                    hasBeenBought: false,
+                                    whoIsTheRecipient: null,
+                                },
+                                {
+                                    itemId: 1,
+                                    name: 'brus',
+                                    amount: 10,
+                                    price: null,
+                                    hasBeenBought: false,
+                                    whoIsTheRecipient: null,
+                                }
+                            ],
+                        },
+                        {
+                            listId: 0,
+                            listType: 'wishlist',
+                            listName: 'ting vi trenger i huset',
+                            usersCanView: [0, 1],
+                            usersCanEdit: [0],
+                            isCompleted: false,
+                            listItems: [
+                                {
+                                    itemId: 0,
+                                    name: 'nytt bord',
+                                    amount: 1,
+                                    price: 4000,
+                                    hasBeenBought: false,
+                                    whoIsTheRecipient: null
+                                }
+                            ]
+                        }
+                    ],
+                }
+            ],
     }
 }
