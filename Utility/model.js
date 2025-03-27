0103b3c2d981b7795a46649b2f2866929d70b0c8
noname0000-blip <augusteide98@hotmail.com>
2025-03-27 13:42:34 +0100

    Update model.js

--- Utility/model.js
+++ Utility/model.js
@@ -108,33 +108,36 @@ const model = {
                                 rank: 3
                             }],
                     }],
-
-                    listLog: {
-                        listType: 'shoppingList',
-                        listName: 'handle til bursdag',
-                        listId: 0,
-                        date: '26/3/2025',
-                        listItems: [
+                    log:
+                        [
                             {
-                                itemId: 0,
-                                name: 'Kake',
-                                amount: 1,
-                                price: null,
-                                hasBeenBought: false,
-                                whoIsTheRecipient: null,
-                            },
-                            {
-                                itemId: 1,
-                                name: 'Coca cola',
-                                amount: 4,
-                                price: null,
-                                hasBeenBought: false,
-                                whoIsTheRecipient: null,
-                            }],
-                    }
+                                listType: 'shoppingList',
+                                listName: 'handle til bursdag',
+                                listId: 0,
+                                date: '26/3/2025',
+                                listItems:
+                                    [
+                                        {
+                                            itemId: 0,
+                                            name: 'Kake',
+                                            amount: 1,
+                                            price: null,
+                                            hasBeenBought: false,
+                                            whoIsTheRecipient: null,
+                                        },
+                                        {
+                                            itemId: 1,
+                                            name: 'Coca cola',
+                                            amount: 4,
+                                            price: null,
+                                            hasBeenBought: false,
+                                            whoIsTheRecipient: null,
+                                        }
+                                    ],
+                            }
+                        ]
                 },
             ],
-
         groups:
             [
                 {
@@ -142,93 +145,98 @@ const model = {
                     name: 'Rubber Duck As',
                     usersId: [0, 1],
                     adminUserId: [0],
-                    shoppingLists: [{
-                        groupShoppingListId: 0,
-                        listName: 'handle til jobbfest',
-                        usersCanView: [0, 1],
-                        usersCanEdit: [0],
-                        isCompleted: false,
-                        listItems: [
+                    lists:
+                        [
                             {
-                                itemId: 0,
-                                name: 'pizza',
-                                amount: 3,
-                                price: null,
-                                hasBeenBought: false,
-                                whoIsTheRecipient: null,
-                            }]
-                    }],
-                    lists: [
-                        {
-                            listId: 0,
-                            listType: 'wishlist',
-                            listName: 'ting vi trenger på jobben',
-                            usersCanView: [0, 1],
-                            usersCanEdit: [0],
-                            isCompleted: false,
-                            listItems: [
-                                {
-                                    itemId: 0,
-                                    name: 'Ny skriver',
-                                    amount: 1,
-                                    price: 10000,
-                                    hasBeenBought: false,
-                                    whoIsTheRecipient: null,
-                                }]
-                        }
-                    ]
+                                listId: 0,
+                                listType: 'wishlist',
+                                listName: 'ting vi trenger på jobben',
+                                usersCanView: [0, 1],
+                                usersCanEdit: [0],
+                                isCompleted: false,
+                                listItems:
+                                    [
+                                        {
+                                            itemId: 0,
+                                            name: 'Ny skriver',
+                                            amount: 1,
+                                            price: 10000,
+                                            hasBeenBought: false,
+                                            whoIsTheRecipient: null,
+                                        }
+                                    ]
+                            },
+                            {
+                                listId: 1,
+                                listType: 'shoppingList',
+                                listName: 'handle til jobbfest',
+                                usersCanView: [0, 1],
+                                usersCanEdit: [0],
+                                isCompleted: false,
+                                listItems: [
+                                    {
+                                        itemId: 0,
+                                        name: 'pizza',
+                                        amount: 3,
+                                        price: null,
+                                        hasBeenBought: false,
+                                        whoIsTheRecipient: null,
+                                    }]
+                            }
+                        ],
                 },
                 {
                     groupId: 1,
                     name: 'Family group',
                     usersId: [0, 1],
                     adminUserId: [0],
-                    lists: [
-                        {
-                            listId: 0,
-                            listType: 'shoppingList',
-                            listName: 'handle til bursdag',
-                            usersCanView: [0, 1],
-                            usersCanEdit: [0],
-                            isCompleted: false,
-                            listItems: [
-                                {
-                                    itemId: 0,
-                                    name: 'iskrem',
-                                    amount: 5,
-                                    price: null,
-                                    hasBeenBought: false,
-                                    whoIsTheRecipient: null,
-                                },
-                                {
-                                    itemId: 1,
-                                    name: 'brus',
-                                    amount: 10,
-                                    price: null,
-                                    hasBeenBought: false,
-                                    whoIsTheRecipient: null,
-                                }
-                            ],
-                        },
-                        {
-                            listId: 0,
-                            listType: 'wishlist',
-                            listName: 'ting vi trenger i huset',
-                            usersCanView: [0, 1],
-                            usersCanEdit: [0],
-                            isCompleted: false,
-                            listItems: [
-                                {
-                                    itemId: 0,
-                                    name: 'nytt bord',
-                                    amount: 1,
-                                    price: 4000,
-                                    hasBeenBought: false,
-                                    whoIsTheRecipient: null
-                                }
-                            ]
-                        }
-                    ],
+                    lists:
+                        [
+                            {
+                                listId: 0,
+                                listType: 'shoppingList',
+                                listName: 'handle til bursdag',
+                                usersCanView: [0, 1],
+                                usersCanEdit: [0],
+                                isCompleted: false,
+                                listItems: [
+                                    {
+                                        itemId: 0,
+                                        name: 'iskrem',
+                                        amount: 5,
+                                        price: null,
+                                        hasBeenBought: false,
+                                        whoIsTheRecipient: null,
+                                    },
+                                    {
+                                        itemId: 1,
+                                        name: 'brus',
+                                        amount: 10,
+                                        price: null,
+                                        hasBeenBought: false,
+                                        whoIsTheRecipient: null,
+                                    }
+                                ],
+                            },
+                            {
+                                listId: 0,
+                                listType: 'wishlist',
+                                listName: 'ting vi trenger i huset',
+                                usersCanView: [0, 1],
+                                usersCanEdit: [0],
+                                isCompleted: false,
+                                listItems: [
+                                    {
+                                        itemId: 0,
+                                        name: 'nytt bord',
+                                        amount: 1,
+                                        price: 4000,
+                                        hasBeenBought: false,
+                                        whoIsTheRecipient: null
+                                    }
+                                ]
+                            }
+                        ],
                 }
             ],
     }
