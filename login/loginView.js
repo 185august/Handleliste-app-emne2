f3e3b4b3c3b3b54b5f3404d034fc06f7378e243c
martinius97 <martiniusr@outlook.com>
2025-03-31 13:29:16 +0200

    new

--- login/loginView.js
+++ login/loginView.js
@@ -1,6 +1,14 @@
 function loginView() {
-    let html = `
-        'hei'
-    `;
-    return html;
+  let loginhtml = /*html*/ `<div class="login-container">
+        <h2>Login</h2>
+        <form id="loginform">
+          <input type="text" id="username" placeholder="Username" required value="${model.input.loginPage.username}"oninput=´model.input.loginPage.username = this.value´ />
+          <input type="password" id="password" placeholder="Password" required value="${model.input.loginPage.password}"oninput=´model.input.loginPage.password = this.value´/>
+          <button type="submit">Login</button>
+          <p id="error-message"></p>
+        </form>
+      </div>
+  
+  `;
+  return loginhtml;
 }
