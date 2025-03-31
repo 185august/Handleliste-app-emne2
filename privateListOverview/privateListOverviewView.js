e9b973a981d6f09f783a060930ed142ee78e4cf5
andwar92 <andwar1992@gmail.com>
2025-03-31 14:27:35 +0200

    Privatliste

--- privateListOverview/privateListOverviewView.js
+++ privateListOverview/privateListOverviewView.js
@@ -1,3 +1,21 @@
 function privateListOverviewView() {
-
+let privateList = '';
+model.data.users[model.app.currentUserId].lists.forEach(List => {
+    privateList += `<div onclick="printPrivateList(${List.listId})">
+    <h1>${List.listName}</h1>
+    <p>
+    ${List.listType}
+    </p>
+    </div>`
+});
+return privateList
 }
+function printPrivateList(id) {
+    let html = `<ul>`;
+    const list = model.data.users[model.app.currentUserId].lists.find((element)=>element.listId == id)
+    list.listItems.forEach(element=>{
+        html+= `<li>${element.name}</li>`
+    })
+    html += `</ul>`
+    console.log(html)
+};
