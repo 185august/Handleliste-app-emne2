9f748e103451435b0ea696c0fc9859cc959779e3
Minsko3333 <alstjd0368@gmail.com>
2025-04-02 20:58:46 +0200

    1

--- settings/settingsView.js
+++ settings/settingsView.js
@@ -1,26 +1,49 @@
 let alreadychanging = false
+let settingsViewhtml = ''
+// 뒤로가기 버튼 동작안함 페이지 오브젝트에 저장하는거 해야할듯 
+
 function settingsView() {
+ if(settingsViewhtml){
+    let html = /*HTML*/`
+    <div class = "page" id = "settingsApp">${settingsViewhtml}
+    </div>`
+  return html
+  }
+  if(!settingsViewhtml){ 
+  let html = /*HTML*/`
+  <div class = "page" id = "settingsApp">
+  <span  onclick="goToPreviousPage(-1)"><b><</b></span>
+    <div style = "text-align: center;">
+     <h3>Bruker innstillinger</h3>
+     <p onclick="profilView()">Profil</p>
+     <p onclick="groupSettingsView()">Grupper</p>
+     <div id = "nameFixInput"></div>
+    </div>`
+    return html
+  }
+  
+    
+}
+
+function profilView(){
+  console.log('🔥 settingsViewhtml 내용:', settingsViewhtml);
   let html = /*HTML*/`
   <div class = "page">
   <span  onclick="goToPreviousPage(-1)"><b><</b></span>
     <div style = "text-align: center;">
      <h3>Profil</h3>
-   
+     <p onclick = "changeName()">Endre navn</p>
+     <p onclick = "changePassword()">Endre passord</p>
+     <p onclick = "removeLogg()">Slett handleliste loggen</p>
   `
-  let Namepage = Name()
-  html += Namepage + `</div>`
   html += `<div id = "nameFixInput"></div>
             </div>`
-  
-  return html
+  settingsViewhtml = html
+  setPage('settings')
 }
-function Name(){
-  if(alreadychanging){
-    return
-  }
-  else{
-  let html = /*HTML*/
-  `<p onclick = "changeName()">Endre navn</p>
-  `
-  return html}
+
+
+function groupSettingsView(){
+
 }
+
