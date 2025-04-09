b9fdfa70907b0108ba3f0239f4c55c81679ad513
martinius97 <martiniusr@outlook.com>
2025-04-09 09:56:14 +0200

    Logginn

--- login/loginController.js
+++ login/loginController.js
@@ -6,6 +6,7 @@ function loginsavepasswordinput(inputpassword) {
 }
 function loginhandler() {
   let errormessage = "";
+
   for (let i = 0; i < model.data.users.length; i++) {
     if (
       model.data.users[i].username == model.input.loginPage.username &&
@@ -13,19 +14,34 @@ function loginhandler() {
     ) {
       //loginsuccess
       errormessage = "loginsuccess";
+      loginsucess = true;
     } else if (
       model.data.users[i].username != model.input.loginPage.username &&
       model.data.users[i].password == model.input.loginPage.password
     ) {
       errormessage = "feil brukernavn";
+      loginsucess = false;
     } else if (
       model.data.users[i].username == model.input.loginPage.username &&
       model.data.users[i].password != model.input.loginPage.password
     ) {
       errormessage = "feil passord";
+      loginsucess = false;
     } else {
       errormessage = "feil brukernavn og passord";
+      loginsucess = false;
     }
   }
   document.querySelector("#error-message").innerHTML = errormessage;
+  if (loginsucess) {
+    setPage("settings");
+  } else {
+    resetlogininputs();
+  }
+}
+function resetlogininputs() {
+  document.querySelector("#username").value = "";
+  document.querySelector("#password").value = "";
+  model.input.loginPage.username = "";
+  model.input.loginPage.password = "";
 }
--- login/loginView.js
+++ login/loginView.js
@@ -4,10 +4,15 @@ function loginView() {
         <form id="loginform">
           <input type="text" id="username" placeholder="Username" onchange="loginsaveusernameinput(this.value)" />
           <input type="password" id="password" placeholder="Password" onchange="loginsavepasswordinput(this.value)"/>
-          <button type="submit" onclick="loginhandler()">Loginn</button>
+          <button type="submit" onclick="handleLogin(event)">Loginn</button>
           <p id="error-message"></p>
         </form>
       </div>
   `;
   return loginhtml;
+}
+
+function handleLogin(event) {
+  event.preventDefault();
+  loginhandler();
 }
