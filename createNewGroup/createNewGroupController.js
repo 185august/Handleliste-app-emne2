a2bda489b8ef652b4f1a54940a969fc906b2804e
noname0000-blip <augusteide98@hotmail.com>
2025-04-22 14:52:01 +0200

    new fix hihi

--- createNewGroup/createNewGroupController.js
+++ createNewGroup/createNewGroupController.js
@@ -1,6 +1,14 @@
 function addUserToGroup() {
     const newMember = model.data.users.find(user => user.username === model.input.createNewGroup.username)
-    if (newMember == undefined || newMember == null) alert('bruker eksisterer ikke');
+    if (newMember == undefined || newMember == null) {
+        alert('bruker eksisterer ikke');
+    }
+    else if (newMember.username == model.data.users.find(obj => obj.userId === model.app.currentUserId).username) {
+        alert('Du er allerede lagt til i gruppen');
+    }
+    else if (model.input.createNewGroup.usersInGroup.includes(newMember.userId)) {
+        alert('Bruker allerede lagt til');
+    }
     else {
         /* const newMemberIndex = model.data.users.findIndex(user=> user.username ===model.input.createNewGroup) */
         model.input.createNewGroup.usersInGroup.push(newMember.userId);
@@ -14,7 +22,7 @@ function initializeGroup() {
         groupId: null,
         name: "tempName",
         usersId: [],
-        adminUserId: null,
+        adminUserId: [],
         showLists: false,
         lists: []
     };
@@ -26,19 +34,20 @@ function createNewGroup() {
         alert('navn eksisterer allerede');
         return;
     } else {
-        const group = model.data.groups[model.data.groups.length - 1]
+        const group = model.data.groups[model.data.groups.length - 1];
+
         model.input.createNewGroup.usersInGroup.push(model.app.currentUserId);
+
         group.groupId = createNewId(model.data.groups, "groupId");
         group.name = model.input.createNewGroup.name;
         group.usersId.push(model.input.createNewGroup.usersInGroup);
-        group.adminUserId = model.app.currentUserId;
+        group.adminUserId.push(model.app.currentUserId);
 
         model.input.createNewGroup.usersInGroup.forEach(id => {
             model.data.users.find(obj => obj.userId == id).groupsId.push(group.groupId);
         });
-
-        model.input.createNewGroup.name = ''
-        model.input.createNewGroup.username = ''
+        model.input.createNewGroup.name = '';
+        model.input.createNewGroup.username = '';
         model.input.createNewGroup.usersInGroup = [];
         goToPreviousPage(-1);
     }
